3bbc070afe69034ec7fef68e02f9a1a8529b0df0
Giulio Gratta <giuliog@stanford.edu>
2016-09-16 11:03:21 -0700

    Move requirements to setup.py

--- setup.py
+++ setup.py
@@ -54,6 +54,10 @@ setup(
     install_requires=[
         'XBlock',
         'xblock-utils',
+        'django',
+        'django_nose',
+        'mock',
+        'coverage',
     ],
     dependency_links=[
         'https://github.com/edx/xblock-utils/tarball/c39bf653e4f27fb3798662ef64cde99f57603f79#egg=xblock-utils',
