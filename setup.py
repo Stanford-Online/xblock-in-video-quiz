6b9e12ee95f3d09a466a56dff7d0ad8f910fbf3d
Giulio Gratta <giuliog@stanford.edu>
2017-02-09 15:02:45 -0800

    push version to v1.0.3

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class Tox(TestCommand):
 
 setup(
     name='invideoquiz-xblock',
-    version='0.1.2',
+    version='0.1.3',
     description='Helper XBlock to locate CAPA problems within videos.',
     license='AGPL v3',
     packages=[
