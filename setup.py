b35af5443263509cfa8b406508879457eac88b35
Iana Vasylieva <ianavasylieva@Ianas-MacBook-Pro.local>
2018-12-17 16:35:04 +0200

    changed required version of django in setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'invideoquiz',
     ],
     install_requires=[
-        'django >= 1.8, < 1.9',
+        'django >= 1.8',
         'django_nose',
         'mock',
         'coverage',
